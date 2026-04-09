9b902f7ac42a7e7df685823481869825d4b0ea19
TuongPham <tuongpvth@gmail.com>
2026-04-09 09:58:23 +0700

    add Either

--- src/exchange/modify.rs
+++ src/exchange/modify.rs
@@ -1,15 +1,19 @@
+use either::Either;
 use serde::{Deserialize, Serialize};
 
 use super::{order::OrderRequest, ClientOrderRequest};
 
+pub type Cloid = String;
+pub type OidOrCloid = Either<u64, Cloid>;
+
 #[derive(Debug)]
 pub struct ClientModifyRequest {
-    pub oid: u64,
+    pub oid: OidOrCloid,
     pub order: ClientOrderRequest,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct ModifyRequest {
-    pub oid: u64,
+    pub oid: OidOrCloid,
     pub order: OrderRequest,
 }
